9b1e8bd29872b254d4938089e482ddcfe3020322
Luis Iñiguez Hernandez <luisangel.iniguez@wizeline.com>
2021-10-04 12:53:22 -0500

    feat: Modified how factory opts are built and lambda hanlder created

--- lib/storage/factory.go
+++ lib/storage/factory.go
@@ -10,18 +10,25 @@ type FactoryOpts struct {
 	FullPath    string
 }
 
-func CreateStorage(opts FactoryOpts) (InternalStorage, error) {
-	if len(opts.StorageType) == 0 {
-		opts.StorageType = os.Getenv("STORAGE_TYPE")
+func NewFactoryOpts(StorageType string, FullPath string) *FactoryOpts {
+	if len(StorageType) == 0 {
+		StorageType = os.Getenv("STORAGE_TYPE")
 	}
 
+	if len(FullPath) == 0 {
+		FullPath = os.Getenv("STORAGE_PATH")
+	}
+
+	return &FactoryOpts{
+		StorageType,
+		FullPath,
+	}
+}
+
+func CreateStorage(opts *FactoryOpts) (InternalStorage, error) {
 	if opts.StorageType == "file" {
 		if len(opts.FullPath) == 0 {
-			opts.FullPath = os.Getenv("STORAGE_PATH")
-
-			if len(opts.FullPath) == 0 {
-				return nil, errors.New("FullPath missing from options")
-			}
+			return nil, errors.New("FullPath missing from options")
 		}
 
 		return FileStorage{opts.FullPath}, nil
--- main.go
+++ main.go
@@ -1,28 +1,57 @@
 package main
 
 import (
+	"context"
+	"crypto/sha256"
 	"downloader/lib/storage"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	log "github.com/sirupsen/logrus"
 )
-
-func main() {
-  log.Info("Getting storage")
-	fs, err := storage.CreateStorage(storage.FactoryOpts{})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fs.Store("hola.txt", []byte("Hola diosito"))
-}
 
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.DebugLevel)
+}
+
+func handler(ctx context.Context, sqsEvent events.SQSEvent) {
+	// This would not work
+	lambda.Start(handler)
+}
+
+func main() {
+	log.Info("Getting storage")
+	fs, err := storage.CreateStorage(storage.NewFactoryOpts("", ""))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := os.Getenv("PROPERTY_URL")
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hashedUrl := fmt.Sprintf("%x", sha256.Sum256([]byte(url)))
+	fileName := fmt.Sprintf("%s.html", hashedUrl)
+	log.WithField("file_name", fileName).Info("Storing file")
+	fs.Store(fileName, body)
 }
 
 func mainFake() {
